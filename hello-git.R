9741299208304be4670e624474679a7abc3a4800
PLAN CHRISTIAN ROMEO CRUZ <plan002@mymail.sim.edu.sg>
2021-09-20 21:11:11 +0800

    testing 3

--- hello-git.R
+++ hello-git.R
@@ -1,3 +1,9 @@
 a <- "Hello Git"
 print(a)
 print(paste(a, "Hello branching!"))
+
+
+install.packages("devtools")
+devtools::install_github("username/packagename")
+
+file.exists("~/.ssh/id_rsa.pub")
